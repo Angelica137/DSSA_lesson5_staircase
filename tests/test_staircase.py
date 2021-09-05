fdd95470bc7ed74e8aab12382f99ba75064ef1ea
Angelica <Angelica137@users.noreply.github.com>
2021-09-05 15:34:07 +0100

    complete test cases, all pass

--- tests/test_staircase.py
+++ tests/test_staircase.py
@@ -15,3 +15,11 @@ def test_staircase_n_3_returns_4():
 
 def test_staircase_n_4_returns_7():
     assert staircase(4) == 7
+
+
+def test_staircase_n_5_returns_13():
+    assert staircase(5) == 13
+
+
+def test_staircase_n_20_returns_121415():
+    assert staircase(20) == 121415
